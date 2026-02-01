9df78dce0a2edfe0c7707d3e4a8ea11c2c242b60
nishan <bhattarainisham@gmail.com>
2026-02-01 18:03:26 +0545

    chore: update dependencies and add Clerk packages
    - Added @clerk/clerk-expo and @clerk/types to dependencies
    - Moved react-native-worklets to the end of dependencies
    - Ensured typescript is listed in devDependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,33 @@
 import { Stack } from "expo-router";
+import React from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
+
+
+const publishableKey =process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
+
+if(!publishableKey){
+  throw new Error(
+    'Missing Publishable key'
+  )
+}
 
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
+    <ClerkProvider publishableKey={publishableKey}>
+    <ClerkLoaded>
+   <SafeAreaProvider>
       <SafeAreaView  style={{
-        flex:1
+        flex:1,
+        backgroundColor:"black"
       }}>
     <Stack screenOptions={{headerShown:false}}/>
       
     </SafeAreaView>
     </SafeAreaProvider>
+    </ClerkLoaded>
+    </ClerkProvider>
+ 
     
   );
 }
