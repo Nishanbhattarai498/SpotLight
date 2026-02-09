408416bcd64c66426dfd8d906d61d69c2ac94538
nishan <bhattarainisham@gmail.com>
2026-02-09 18:11:28 +0545

    feat: update Clerk package to version 2.19.22 and implement token caching with SecureStore

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,33 +1,31 @@
 import { Stack } from "expo-router";
-import React from "react";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
+import { SafeAreaProvider,SafeAreaView } from "react-native-safe-area-context";
+import {ClerkProvider,ClerkLoaded} from "@clerk/clerk-expo";
+import { tokenCache } from "@/cache";
+ 
 
-
-const publishableKey =process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
-
+const publishableKey=process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 if(!publishableKey){
   throw new Error(
-    'Missing Publishable key'
-  )
+    "Missing EXPO PUBLISHABLE KEY"
+  );
+
 }
 
-export default function RootLayout() {
-  return (
-    <ClerkProvider publishableKey={publishableKey}>
-    <ClerkLoaded>
-   <SafeAreaProvider>
-      <SafeAreaView  style={{
-        flex:1,
-        backgroundColor:"black"
-      }}>
-    <Stack screenOptions={{headerShown:false}}/>
-      
-    </SafeAreaView>
-    </SafeAreaProvider>
-    </ClerkLoaded>
+export default function RootLayout(){
+  return(
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <SafeAreaProvider>
+          <SafeAreaView style={{flex:1,backgroundColor:"#000"}}>
+            <Stack screenOptions={{headerShown:false}}>
+
+            </Stack>
+
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </ClerkLoaded>
+
     </ClerkProvider>
- 
-    
-  );
+  )
 }
